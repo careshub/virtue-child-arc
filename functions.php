5a9f797d8295fa9db6a6a097c05a28ddfb69a85b
David Cavins <david.cavins@gmail.com>
2018-02-13 17:11:29 -0600

    Handle the Bootstrap conflict.
    Remove the `virtue_plugins` js file that includes Bootstrap 3 in it. Add the other parts back to an alternative js file so that the theme will continue to work.

--- functions.php
+++ functions.php
@@ -29,8 +29,14 @@ add_filter( 'body_class', function( $classes ) {
 function virtue_child_arc_scripts() {
 	// Include the needed js file.
 	wp_enqueue_script( 'virtue-child-arc-base-scripts', get_theme_file_uri( '/js/public.js' ), array( 'jquery' ), '1.0.1', true );
+
+	// Dequeue the Virtue theme "plugins" script on the reports page
+	if ( is_page( array( 'report', 'interactive-report' ) ) ) {
+		wp_dequeue_script( 'virtue_plugins' );
+		wp_enqueue_script( 'virtue-plugins-alt', get_theme_file_uri( '/js/virtue-alt-plugins.js' ), array( 'jquery' ), '1.0.1', true );
+	}
 }
-add_action( 'wp_enqueue_scripts', 'virtue_child_arc_scripts' );
+add_action( 'wp_enqueue_scripts', 'virtue_child_arc_scripts', 999 );
 
 /**
  * Don't show the footer logo bar on the about page.
@@ -51,3 +57,5 @@ function arc_hide_logo_bar_on_about_page( $instance, $widget, $args ) {
 
 }
 add_filter( 'widget_display_callback', 'arc_hide_logo_bar_on_about_page', 10, 3 );
+
+
